c7411ecda6ee9b7ef65a8e9d93e4d295018b636d
stefan <sa.stefan.andjelkovic@gmail.com>
2017-11-06 19:10:17 +0100

    Initial commit

---
+++ src/main/kotlin/com/sandjelkovic/kombinator/KombinatorApplication.kt
@@ -0,0 +1,11 @@
+package com.sandjelkovic.kombinator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KombinatorApplication
+
+fun main(args: Array<String>) {
+    runApplication<KombinatorApplication>(*args)
+}
