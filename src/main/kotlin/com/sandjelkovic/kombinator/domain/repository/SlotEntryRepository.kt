26e4b7b90d4ad06ff6091cea84fd046f45a237fa
stefan <sa.stefan.andjelkovic@gmail.com>
2017-11-19 12:09:55 +0100

    Add example test data in the database

---
+++ src/main/kotlin/com/sandjelkovic/kombinator/domain/repository/SlotEntryRepository.kt
@@ -0,0 +1,11 @@
+package com.sandjelkovic.kombinator.domain.repository
+
+import com.sandjelkovic.kombinator.domain.model.SlotEntry
+import org.springframework.data.repository.CrudRepository
+
+/**
+ * @author sandjelkovic
+ * @date 19.11.17.
+ */
+interface SlotEntryRepository  : CrudRepository<SlotEntry, Long> {
+}
---
+++ src/main/kotlin/com/sandjelkovic/kombinator/domain/repository/SlotRepository.kt
@@ -0,0 +1,11 @@
+package com.sandjelkovic.kombinator.domain.repository
+
+import com.sandjelkovic.kombinator.domain.model.Slot
+import org.springframework.data.repository.CrudRepository
+
+/**
+ * @author sandjelkovic
+ * @date 19.11.17.
+ */
+interface SlotRepository : CrudRepository<Slot, Long> {
+}
