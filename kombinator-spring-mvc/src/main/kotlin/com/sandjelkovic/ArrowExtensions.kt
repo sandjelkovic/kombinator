5d138675c5e092dc64a0d82b903ba6d4b552d242
StefanAndjelkovic <sa.stefan.andjelkovic@gmail.com>
2019-01-04 11:13:51 +0100

    Fix Nothing to Inline Kotlin warning

--- kombinator-spring-mvc/src/main/kotlin/com/sandjelkovic/ArrowExtensions.kt
+++ kombinator-spring-mvc/src/main/kotlin/com/sandjelkovic/ArrowExtensions.kt
@@ -8,5 +8,5 @@ import java.util.*
  * @author sandjelkovic
  * @date 6.9.18.
  */
-inline fun <T> Optional<T>.flatMapToOption(): Option<T> = this.map { it.toOption() }.orElse(Option.empty())
+fun <T> Optional<T>.flatMapToOption(): Option<T> = this.map { it.toOption() }.orElse(Option.empty())
 
